fef0b5181d37d03726edc539904380de2eb891bf
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2024-05-27 21:47:46 -0300

    atualizando as telas, configurando as imagens

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Gerenciador-de-acervo
-Um programa utilizado para gerenciar estoque bibliotecario
+Um programa utilizado para gerenciar um estoque bibliotecario
 o repositorio contém os codigos criação do banco de dados
 inserção dos dados nas tabelas
 Também contém os codigos para geração da parte grafica do programa
--- telas.py
+++ telas.py
@@ -8,13 +8,50 @@ C_Branco = "#feffff"
 #configurando o titulo o tamanho e cor de fundo da janela
 janela = Tk()
 janela.title("Meu Acervo")
-janela.geometry("800x360")
-janela.configure(background=C_Branco)
+janela.geometry("770x330")
+janela.configure(background = C_Branco)
 
 style = Style(janela)
 style.theme_use("clam")
 
-cabecalho = Frame(janela, width=800, height=60, background=C_Azul, relief="flat")
+#configurando as imagens
+#imagem do logo
+logo_img = Image.open("icons/logo.jpg")
+logo_img = logo_img.resize((40,40))
+logo_img = ImageTk.PhotoImage(logo_img)
+
+#imagem botão adicionar livro
+add_livro_img = Image.open("icons/lotr.jpg")
+add_livro_img = add_livro_img.resize(18,18)
+add_livro_img = ImageTk.PhotoImage(add_livro_img)
+
+#imagem botão adicionar quadrinho
+add_quadrinho_img = Image.open("icons/lanterna.jpg")
+add_quadrinho_img = add_quadrinho_img.resize(18,18)
+add_quadrinho_img = ImageTk.PhotoImage(add_quadrinho_img)
+
+#imagem botão deletar livro
+del_livro_img = Image.open("icons/naruto.jpg")
+del_livro_img = add_livro_img.resize(18,18)
+del_livro_img = ImageTk.PhotoImage(add_livro_img)
+
+#configurando as partes da tela 
+cabecalho = Frame(janela, width=770, height=50, background=C_Azul, relief="flat")
 cabecalho.grid(row=0, column=0, columnspan=2, sticky=NSEW)
+
+#menu
+menu_lateral = Frame(janela, width=150, height=265, background=C_Azul, relief="solid")
+menu_lateral.grid(row = 1, column=0, sticky=NSEW)
+
+principal = Frame(janela, width=600, height=265, background=C_Branco, relief="raised")
+principal.grid(row=1, column=1, sticky=NSEW)
+
+app_logo = Label(cabecalho, image=logo_img, width=1000, compound=LEFT, padx=5, anchor=NW, background=C_Azul, fg=C_Branco)
+app_logo.place(x=5, y=0)
+
+app_logo_texto = Label(cabecalho, text="Acerco", compound=LEFT, padx=5, anchor=NW, font="Verdana 15 bold",background=C_Azul, fg=C_Branco)
+app_logo_texto.place(x=50, y=7)
+
+#botões do menu
 
 janela.mainloop()
